b6c91842f8840f4d08e880e3c8f03c20b849a4fe
Danh Vu Le <69585816+littlelovevu@users.noreply.github.com>
2021-11-24 21:01:24 +0700

    .

--- TASK.WebApp/obj/Debug/net5.0/RazorDeclaration/Pages/Dashboard.razor.g.cs
+++ TASK.WebApp/obj/Debug/net5.0/RazorDeclaration/Pages/Dashboard.razor.g.cs
@@ -13,91 +13,91 @@ namespace TASK.WebApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 1 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 2 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 3 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 4 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 5 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 6 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 7 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 8 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 9 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using TASK.WebApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 10 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using TASK.WebApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 11 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Radzen;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\_Imports.razor"
+#line 12 "D:\ASC\BaiTapLon\TASK.WebApp\_Imports.razor"
 using Radzen.Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\Pages\Dashboard.razor"
+#line 2 "D:\ASC\BaiTapLon\TASK.WebApp\Pages\Dashboard.razor"
 using System.Globalization;
 
 #line default
@@ -112,7 +112,7 @@ using System.Globalization;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 23 "C:\Users\HoangLong\source\repos\TASK_PROJECT\TASK.WebApp\Pages\Dashboard.razor"
+#line 23 "D:\ASC\BaiTapLon\TASK.WebApp\Pages\Dashboard.razor"
        
     class DataItem
     {
